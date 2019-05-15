fd164634dde3730828122e78b4cd3b2b3d3c1e67
st118208 <st118208@stud.uni-stuttgart.de>
2019-05-15 12:27:17 +0200

    Add files via upload
    Kommentare hinzugefügt.
    Offset Array hinzugefügt.

--- src/Graph.java
+++ src/Graph.java
@@ -4,12 +4,30 @@ import java.util.Scanner;
 import java.util.Locale;
 import java.io.FileNotFoundException;
 
+/**
+ * Die Klasse Graph liest die verschiedenen Graphen ein.
+ * In Nodes werden die Knoten mit Länge und Breite gespeichert.
+ * In Vertices werden die Kanten mit Startknoten, Zielknoten und Kosten gespeichert.
+ * In OffsetArray wird der Offset, die erste Kante, die von einem Knoten abgeht, gespeichert.
+ * numberOfNodes ist die Anzahl der Knoten
+ * numberOfVertices ist die Anzahl der Kanten
+ *
+ *
+ * */
 public class Graph {
     private int[] OffsetArray;
     private float[][] Nodes;
-    private float[][] Vertices;
+    private float[][] Vertices; // ???? Ecken?
     private int numberOfNodes;
     private int numberOfVertices;
+
+    /**
+     * readGraphData liest die Datei des Graphen ein und speichert die Daten in den Arrays.
+     * BufferedReader liest das File ein.
+     * Dann werden die ersten Zeilen der Datei uebersprungen.
+     * ein Scanner wird erstellt um durch die Zeilen zu gehen.
+     * Falls die Datei nicht gefunden wird, wird eine Exeption geworfen.
+     * */
 
     public void readGraphData(String fileName) {
         BufferedReader reader = null;
@@ -57,14 +75,47 @@ public class Graph {
             System.out.println(e.getMessage());
         }
     }
-    public void printNodes(){
-        for(int i = 0; i < numberOfNodes; i++){
-            System.out.println("Node ID: " + i + " | x: " + Nodes[i][0] + " | y: " + Nodes[i][1]);
+
+    /**
+     * calculateOffset erstellt das Offset Array
+     * */
+
+
+    public void calculateOffset() {
+        OffsetArray = new int[numberOfNodes];
+        for (int i = 0; i < numberOfVertices; i++) {
+            if (Vertices[i][0] == i){
+                OffsetArray[(int) Vertices[i][0]]=i;
+            }
+            if(i>0 && (Vertices[i][0] != Vertices[i-1][0])){
+                OffsetArray[(int)Vertices[i][0]] =    i;
+            }
+
         }
+
+
+
     }
+
+    /**
+     * Hilfsmethode zur Ausgabe
+     * */
+        public void printNodes(){
+            for(int i = 0; i < numberOfNodes; i++){
+               // System.out.println("Node ID: " + i + " | x: " + Nodes[i][0] + " | y: " + Nodes[i][1]);
+                System.out.println("Offset: "+ i+ "  Eintrag "+ OffsetArray[i]);
+            }
+        }
+
+
+    /**
+     * Main
+     * */
         public static void main (String[]args){
             Graph graph = new Graph();
             graph.readGraphData("toy.fmi");
+            graph.calculateOffset();
             graph.printNodes();
+
         }
     }
