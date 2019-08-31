24cd3c6e74231182ddecc619a626068b840c752a
st118208 <st118208@stud.uni-stuttgart.de>
2019-08-31 13:38:19 +0200

    Added the libary com.google.code.gson:gson:2.8.5
    - fixed list
    - com.google.code.gson:gson:2.8.5 added to module
    - added private Graph graph = new Graph("MV.fmi");
    to see graph

--- src/main/java/web/utility/ContextListener.java
+++ src/main/java/web/utility/ContextListener.java
@@ -1,13 +1,17 @@
 package web.utility;
 
-import roupla.utility.*;
+import com.google.gson.Gson;
+import roupla.utility.Graph;
+import roupla.utility.Hub;
 
 import javax.servlet.ServletConfig;
-import javax.servlet.annotation.WebListener;
-import javax.servlet.ServletContextListener;
-import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContext;
-import javax.servlet.http.*;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -15,7 +19,8 @@ import java.util.List;
 @WebListener
 public class ContextListener extends HttpServlet implements ServletContextListener {
     private ServletContext context = null;
-    private Hub hub = new Hub("bw.fmi");
+    private Hub hub = new Hub("MV.fmi");
+    private Graph graph = new Graph("MV.fmi");
     public void init(ServletConfig config) throws javax.servlet.ServletException {
         //initializing
         super.init(config);
@@ -38,12 +43,12 @@ public class ContextListener extends HttpServlet implements ServletContextListen
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) {
-             
+
     }
 
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) {
-   try {
+        try {
             String input = request.getReader().readLine();
             System.out.println(input);
             String[] coordinates = input.split("[(, )]");
@@ -52,24 +57,24 @@ public class ContextListener extends HttpServlet implements ServletContextListen
                     Double.parseDouble(coordinates[1]), Double.parseDouble(coordinates[3]), Double.parseDouble(coordinates[5]), Double.parseDouble(coordinates[7]));
             System.out.println("done" + path.toString() + " done");
             //Knotenindizes -> koordinatenpaare (lat/long) in string
-            List<Double> LatLong = new List(2* path.length);
-            for (int i =0; i <= LatLong.length(); i +=2){
-                LatLong[i] = graph.getNodes()[0][path[i]];  // lat is 0
-                LatLong[i+1] = graph.getNodes()[1][path[i+1]]; // long is 1
+            List<Double> LatLong = new ArrayList<Double>();
+            for (int i =0; i <= LatLong.size(); i +=2){
+                LatLong.set(i, graph.getNodes()[0][path.get(i)]);  // lat is 0
+                LatLong.set(i + 1, graph.getNodes()[1][path.get(i + 1)]); // long is 1
             };
-            
+
             //FALLS LatLong nicht mit Json funktioniert hier der Stringbuilder:
             //object of stringbuilder class
            /* Stringbuilder builder = new Stringbuilder();
-            
+
             for(Double d: str){
                 builder.append(d);
                 builder.append(" "); // adds space to split it later
             }
-            
+
             String string = builder.toString();
             */
-            
+
             // LatLong muss dann mit string geaendert werden.
             String json = new Gson().toJson(LatLong);
 
